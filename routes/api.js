3fb370a93a55bfd9021b10a6ec02ef8eb6bf2c1f
Christopher McCormack <mccormack.christopher@gmail.com>
2018-10-07 11:31:57 -0700

    Added validations, working on fetch request for stock quote

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 const router = require("express").Router()
-const { body, param, validationResult, } = require('express-validator/check')
-const { sanitizeBody, sanitizeParam } = require('express-validator/filter')
-const Book = require('../models/Book')
-const ObjectId = require('mongoose').Types.ObjectId
+const { query, validationResult, } = require('express-validator/check')
+const { sanitizeQuery } = require('express-validator/filter')
+const fetch = require('node-fetch')
+const Ticker = require('../models/Ticker')
 
 
 module.exports = () => {
@@ -10,8 +10,8 @@ module.exports = () => {
   ///////////////////////////////////////////////////////////
   // Utility Endpoints
   ///////////////////////////////////////////////////////////
-  router.get("/wipebooks", (req, res) => {
-    Book.deleteMany({}, err => {
+  router.get("/wipeticker", (req, res) => {
+    Ticker.deleteMany({}, err => {
       if (err) return Error(err.message)
       const message = "Successfully wiped 'books' collection"
       res.json({ success: true, message, })
@@ -22,176 +22,72 @@ module.exports = () => {
   ///////////////////////////////////////////////////////////
   // Validations
   ///////////////////////////////////////////////////////////
-  const book_title_validation = [
-    body('title')
-      .trim()
-      .isLength({ min: 1, })
-        .withMessage('Book Title missing')
+  const parseQuery = (req, res, next) => {
+
+    const { stock } = req.query
+
+    if (!req.query.stock) {
+      return next(Error('Must include valid stock in request'))
+    }
+
+    if (typeof req.query.stock === 'string') {
+      req.query.stock = [req.query.stock]
+    }
+
+    next()
+  }
+
+
+  const stock_ticker_validation = [
+    query('stock')
+      .isArray()
+      .withMessage('stock query must be an array')
+      .custom(q => q.length >= 1)
+      .withMessage('Must include valid stock in query')
+      .custom(q => q.length <= 2)
+      .withMessage('Maximum of two stocks permitted'),
+
+    query('stock.*')
+      .isLength({ min: 4, max: 5 })
+      .withMessage('Stock Ticker invalid, should be four or five characters')
       .isAscii()
-        .withMessage('Book Title should include only valid ascii characters'),
+      .withMessage('Stock Ticker should include only valid ascii characters'),
 
-    sanitizeBody('title').trim(),
-  ]
-
-  const book_comment_validation = [
-    body('comment')
-      .trim()
-      .isLength({ min: 1, })
-        .withMessage('Book Comment missing')
-      .isAscii()
-        .withMessage('Book Comment should include only valid ascii characters'),
-
-    sanitizeBody('comment').trim(),
-  ]
-
-  const book_id_validation = [
-    param('_id')
-      .trim()
-      .isMongoId()
-        .withMessage('Book ID is not a valid MongoID'),
-
-    sanitizeParam('_id').trim(),
+    sanitizeQuery('stock.*').trim(),
   ]
 
 
   ///////////////////////////////////////////////////////////
-  // Manage Books Add/Update/Delete
+  // Manage Stock Prices GET
   ///////////////////////////////////////////////////////////
-  router.route('/books')
+  router.route('/stock-prices')
 
     // ** GET ** request
-    .get((req, res, next) => {
+    .get(parseQuery, stock_ticker_validation, (req, res, next) => {
 
-      Book.find({}, 'title _id commentcount', (err, books) => {
-        if (err) { 
-          return next(Error(err.message))
-        }
-
-        res.json(books)
-      })
-    })
-
-
-    // ** POST ** request
-    .post(book_title_validation, (req, res, next) => {
-
-      // Check validation and exit early if unsuccessful 
       const errors = validationResult(req)
       if (!errors.isEmpty()) {
+        // console.log('errors', errors.array())
         return next(Error(errors.array()[0].msg))
       }
 
-      new Book({
-        title: req.body.title
-      }).save((err, doc) => {
-        if (err) {
-          return next(Error(err))
-        }
+      const stocks = req.query.stock.map(stock => {
+        console.log(stock)
+        fetch(`https://api.iextrading.com/1.0/stock/${stock}/quote`)
+          .then(data => data.json())
+          .then(data => res.json(data))
+        // Ticker.findOneAndUpdate(
+        //   {
+        //     ticker: stock
+        //   }, 
+        //   {
 
-        res.json({ title: doc.title, _id: doc._id })
-      })
-    })
-
-
-    // ** DELETE ** request
-    .delete((req, res, next) => {
-
-      Book.deleteMany({}, (err, book) => {
-
-        if (err) {
-          return next(Error('could not delete books'))
-        }
-
-        res.json({success: true, message: 'complete delete successful'})
-      })
-    })
-
-
-  router.route('/books/:_id')
-
-
-    // ** GET ** request
-    .get(book_id_validation, (req, res, next) => {
-
-      // Check validation and exit early if unsuccessful 
-      const errors = validationResult(req)
-      if (!errors.isEmpty()) {
-        return next(Error(errors.array()[0].msg))
-      }
-
-      const { _id } = req.params
-
-      Book.findById(req.params._id, 'title _id comments')
-      .exec((err, book) => {
-        if (err) {
-          return next(Error(err))
-        }
-
-        if (!book) {
-          return next(Error(`Book with _id ${_id} not found`))
-        }
-
-        res.json(book)
-      })
-
-    })
-
-
-    // ** POST ** request
-    .post(book_comment_validation, book_id_validation, (req, res, next) => {
-
-      // Check validation and exit early if unsuccessful 
-      const errors = validationResult(req)
-      if (!errors.isEmpty()) {
-        return next(Error(errors.array()[0].msg))
-      }
-
-      const { _id } = req.params
-      const { comment } = req.body
-
-      Book.findByIdAndUpdate(_id,
-        {
-          $push: { comments: comment },
-          $inc: { commentcount: 1 },
-        },
-        {
-          new: true,
-          select: 'title comments'
-        },
-      )
-        .exec((err, doc) => {
-          if (err) {
-            return next(Error(err))
-          }
-          if (!doc) {
-            return next(Error(`Book with _id ${_id} not found`))
-          }
-
-          res.json(doc)
-        })
-    })
-
-
-    // ** DELETE ** request
-    .delete(book_id_validation, (req, res, next) => {
-
-      const { _id } = req.params
-
-      if (!_id) {
-        return next(Error('_id error'))
-      }
-
-      if (!validationResult(req).isEmpty()) {
-        return next(Error('no book exists'))
-      }
-
-      Book.findByIdAndRemove(_id, (err, book) => {
-
-        if (err || !book) {
-          return next(Error('no book exists'))
-        }
-
-        res.json({success: true, message: 'delete successful'})
+        //   },
+        //   { 
+        //     upsert: true,
+        //     new: true
+        //   }
+        // )
       })
     })
 
