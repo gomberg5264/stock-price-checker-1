48a46f74f94b52d02ba070cb71370c1ada964bee
Christopher McCormack <mccormack.christopher@gmail.com>
2018-10-07 18:51:21 -0700

    Completed all tests and added a few extra

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,12 +6,21 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const assert = chai.assert;
+const server = require('../server');
+const Ticker = require('../models/Ticker')
 
 chai.use(chaiHttp);
+
+before(done => {
+  Ticker.deleteMany({}, err => {
+    if (err) console.error(err)
+    console.log("Deleted all documents in `tickers` collection!")
+    done()
+  })
+})
 
 suite('Functional Tests', function() {
     
@@ -20,29 +29,103 @@ suite('Functional Tests', function() {
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog'})
+        .query({stock: 'aapl'})
         .end(function(err, res){
           assert.ok(res.status)
-          console.log('body', res.body)
+          assert.property(res.body, 'stockData', 'Response must include \'stockData\' property')
+          assert.property(res.body.stockData, 'stock', 'stockData must include \'stock\' property')
+          assert.property(res.body.stockData, 'likes', 'stockData must include \'likes\' property')
+          assert.property(res.body.stockData, 'price', 'stockData must include \'price\' property')
+          assert.equal(res.body.stockData.stock, 'AAPL')
+          assert.equal(res.body.stockData.likes, '0')
+          assert.isNumber(res.body.stockData.price)
           done();
         });
       });
       
-      // test('1 stock with like', function(done) {
-        
-      // });
+      test('1 stock with like', function(done) {
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({ stock: 'aapl', like: true})
+          .end(function (err, res) {
+            assert.ok(res.status)
+            assert.property(res.body, 'stockData', 'Response must include \'stockData\' property')
+            assert.property(res.body.stockData, 'stock', 'stockData must include \'stock\' property')
+            assert.property(res.body.stockData, 'likes', 'stockData must include \'likes\' property')
+            assert.property(res.body.stockData, 'price', 'stockData must include \'price\' property')
+            assert.equal(res.body.stockData.stock, 'AAPL')
+            assert.equal(res.body.stockData.likes, '1')
+            assert.isNumber(res.body.stockData.price)
+            done();
+          });
+      });
       
-      // test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
-      // });
+      test('1 stock with like again (ensure likes arent double counted)', function(done) {
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({ stock: 'aapl', like: true })
+          .end(function (err, res) {
+            assert.ok(res.status)
+            assert.property(res.body, 'stockData', 'Response must include \'stockData\' property')
+            assert.property(res.body.stockData, 'stock', 'stockData must include \'stock\' property')
+            assert.property(res.body.stockData, 'likes', 'stockData must include \'likes\' property')
+            assert.property(res.body.stockData, 'price', 'stockData must include \'price\' property')
+            assert.equal(res.body.stockData.stock, 'AAPL')
+            assert.equal(res.body.stockData.likes, '1')
+            assert.isNumber(res.body.stockData.price)
+            done();
+          })
+      })
       
-      // test('2 stocks', function(done) {
-        
-      // });
+      test('2 stocks', function(done) {
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({ stock: ['csco', 'msft'] })
+          .end(function (err, res) {
+            assert.ok(res.status)
+            assert.property(res.body, 'stockData', 'Response must include \'stockData\' property')
+            const {stockData: data} = res.body
+            assert.isArray(data)
+            assert.property(data[0], 'stock', 'stockData must include \'stock\' property')
+            assert.property(data[0], 'rel_likes', 'stockData must include \'rel_likes\' property')
+            assert.property(data[0], 'price', 'stockData must include \'price\' property')
+            assert.property(data[1], 'stock', 'stockData must include \'stock\' property')
+            assert.property(data[1], 'rel_likes', 'stockData must include \'rel_likes\' property')
+            assert.property(data[1], 'price', 'stockData must include \'price\' property')
+            assert.equal(data[0].stock, 'CSCO')
+            assert.equal(data[0].rel_likes, '0', 'rel_likes value should be 0')
+            assert.isNumber(data[0].price)
+            assert.equal(data[1].stock, 'MSFT')
+            assert.equal(data[1].rel_likes, '0', 'rel_likes value should be 0')
+            assert.isNumber(data[1].price)
+            done();
+          });
+      });
       
-      // test('2 stocks with like', function(done) {
-        
-      // });
+      test('2 stocks with like', function(done) {
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({ stock: ['aapl', 'msft'], like: true })
+          .end(function (err, res) {
+            assert.ok(res.status)
+            assert.property(res.body, 'stockData', 'Response must include \'stockData\' property')
+            const { stockData: data } = res.body
+            assert.isArray(data)
+            assert.property(data[0], 'stock', 'stockData must include \'stock\' property')
+            assert.property(data[0], 'rel_likes', 'stockData must include \'rel_likes\' property')
+            assert.property(data[0], 'price', 'stockData must include \'price\' property')
+            assert.property(data[1], 'stock', 'stockData must include \'stock\' property')
+            assert.property(data[1], 'rel_likes', 'stockData must include \'rel_likes\' property')
+            assert.property(data[1], 'price', 'stockData must include \'price\' property')
+            assert.equal(data[0].stock, 'AAPL')
+            assert.equal(data[0].rel_likes, '0', 'rel_likes value should be 0')
+            assert.isNumber(data[0].price)
+            assert.equal(data[1].stock, 'MSFT')
+            assert.equal(data[1].rel_likes, '0', 'rel_likes value should be 0')
+            assert.isNumber(data[1].price)
+            done();
+          });
+      });
 
       test('0 stocks', function(done) {
         chai.request(server)
@@ -54,6 +137,20 @@ suite('Functional Tests', function() {
             assert.property(res.body, 'error', 'Response must have \'error\' property')
             assert.isFalse(res.body.success, 'Response success must be false')
             assert.equal(res.body.error, 'Must include valid stock in request')
+            done()
+          })
+      })
+
+      test('no duplicate stocks with when two are passed', function (done) {
+        chai.request(server)
+          .get('/api/stock-prices')
+          .query({stock: ['aapl', 'aapl']})
+          .end((err, res) => {
+            assert.ok(res.status)
+            assert.property(res.body, 'success', 'Response must have \'success\' property')
+            assert.property(res.body, 'error', 'Response must have \'error\' property')
+            assert.isFalse(res.body.success, 'Response success must be false')
+            assert.equal(res.body.error, 'Stocks symbols must be unique when comparing')
             done()
           })
       })
