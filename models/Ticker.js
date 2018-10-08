2a96e9dab4000887103608bc2c89dede12e21bd2
Christopher McCormack <mccormack.christopher@gmail.com>
2018-10-07 18:50:20 -0700

    Added getPublicFields method

--- models/Ticker.js
+++ models/Ticker.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose")
 
 const tickerSchema = new mongoose.Schema({
-  ticker: { type: String, required: true },
+  stock: { type: String, required: true },
   ip_addresses: [String],
   likes: { type: Number, default: 0},
+  price: { type: Number, default: 0},
 })
+
+tickerSchema.methods.getPublicFields = function () {
+  return {
+    stock: this.stock,
+    likes: this.likes,
+    price: this.price
+  }
+}
 
 const Ticker = mongoose.model("Ticker", tickerSchema, "tickers")
 
